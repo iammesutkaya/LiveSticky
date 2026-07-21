1899e9269abc1f9095a9dc8b3d7b5baad229aa00
Mesut <hello@mesutkaya.de>
2026-07-21 12:08:26 +0200

    chore: bump app and website version to v1.1.197

--- docs/demo/default.js
+++ docs/demo/default.js
@@ -5,7 +5,7 @@
       </svg>
       <span class="rtc-comments-wrap">
         <span class="rtc-num">0</span>
-        <svg class="rtc-msg-icon" width="11" height="11" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3.5" stroke-linecap="round" stroke-linejoin="round">
+        <svg class="rtc-msg-icon" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
           <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z"/>
         </svg>
       </span>
